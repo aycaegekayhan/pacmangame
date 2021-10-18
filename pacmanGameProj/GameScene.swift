646f697164ccf0a24dd4b11d3e00d9995842f17e
Ayça ege Kayhan <aycaegekayhan@aycas-air.home>
2021-10-17 20:49:01 -0400

    pacman move added, strawberry and apple added to game

--- pacmanGameProj/GameScene.swift
+++ pacmanGameProj/GameScene.swift
@@ -82,7 +82,16 @@ class GameScene: SKScene {
     
     func touchMoved(toPoint pos : CGPoint) {
         
+        
     }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+            for touch in touches {
+                let location = touch.location(in: self)
+                thePacMan.position.x = location.x
+                thePacMan.position.y = location.y
+            }
+        }
     
     func touchUp(atPoint pos : CGPoint) {
        
@@ -92,9 +101,6 @@ class GameScene: SKScene {
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
